6b2bf4d7dd2afc34086f5b94927ac8ce0130b636
Nick <nimamshah@ufl.edu>
2016-01-18 10:54:29 -0500

    Cleaned up JSONtoMongo for proper solution

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -15,24 +15,8 @@ mongoose.connect('config')
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
 */
-var listings
-fs.readFile('listings.json', 'utf8', function(err, data) {
-    /*
-        This callback function should save the data in the listingData variable, 
-        then start the server. 
-    */
-    listings = data;
-    console.log(listings);
-});
-console.log(listings);
-/*
-Listing.collection.insertMany(listings, function(err, r) {
-  assert.equal(null, err);
-  assert.equal(3, r.insertedCount);
 
-  db.close();
-})
-*/
+
 /* 
   Once you've written + run the script, check out your MongoLab database to ensure that 
   it saved everything correctly. 
